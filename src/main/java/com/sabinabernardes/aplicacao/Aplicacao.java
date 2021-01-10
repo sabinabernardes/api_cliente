a7da362180bfbad713f4b44a120b6f0fb5648580
sabinabernardes <sabina.ricardok@gmail.com>
2021-01-10 11:58:07 -0300

    classe cliente

---
+++ src/main/java/com/sabinabernardes/aplicacao/Aplicacao.java
@@ -0,0 +1,13 @@
+package com.sabinabernardes.aplicacao;
+
+import com.sabinabernardes.dominio.Cliente;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+	
+		Cliente p1 = new Cliente (1,"Sabina","sabina@mail","10234578912",null);
+
+	}
+
+}
---
+++ src/main/java/com/sabinabernardes/dominio/Cliente.java
@@ -0,0 +1,121 @@
+package com.sabinabernardes.dominio;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.xml.crypto.Data;
+
+
+
+@Entity
+public class Cliente implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String email;
+	private String cpf;
+	private Data dataNasc;
+	
+	
+	public Cliente () {
+		
+	}
+
+
+	public Cliente(Integer id, String name, String email, String cpf, Data dataNasc) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.cpf = cpf;
+		this.dataNasc = dataNasc;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	public Data getDataNasc() {
+		return dataNasc;
+	}
+
+
+	public void setDataNasc(Data dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
